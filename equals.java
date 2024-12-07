9c3c22c8b050ce8f8ffbccf96b6ac025ac591323
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-07 12:20:36 +0530

    Add files via upload

---
+++ equals.java
@@ -0,0 +1,21 @@
+public class equals
+{
+    public static void main(String ags[])
+{
+        int a=1;
+        int b=2;
+        int c=3;
+
+    {
+        {
+            System.out.println("a+b is equal to c:"+(a+b));        
+        }
+        {
+            System.out.println("a-c is equal to b:"+(c-a));
+        
+    }
+    
+    
+}
+}
+}
